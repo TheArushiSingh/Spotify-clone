7299cad31e1e74f0d265c1936bb8872e1226e0f5
TheArushiSingh <42888907+TheArushiSingh@users.noreply.github.com>
2022-06-24 02:23:39 +0530

    Add files via upload

---
+++ build/precache-manifest.24a6c2f521c65a25e4f144a23afad78c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c482255d4c19ad8d3c5d79bd8cf37e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e0f61782068b71b6f64",
+    "url": "/static/css/main.3dc43085.chunk.css"
+  },
+  {
+    "revision": "4a62fb948ff3a4923191",
+    "url": "/static/js/2.de860d30.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.de860d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0f61782068b71b6f64",
+    "url": "/static/js/main.b4930112.chunk.js"
+  },
+  {
+    "revision": "365a7a989fa41d5c1e8c",
+    "url": "/static/js/runtime-main.cca8ae76.js"
+  }
+]);
